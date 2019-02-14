005dd5fbd2cafe92025df782c00a40acd9f26816
Jarlonyan <286568450@qq.com>
2019-02-15 00:23:04 +0800

    update

--- dog_cat_classifier/config.py
+++ dog_cat_classifier/config.py
@@ -19,3 +19,13 @@ class DefaultConfig(object):
     lr = 0.1
     lr_decay = 0.95
     weight_decay = 1e-4
+
+    def parse(self, kwargs):
+        for k,v in kwargs.items():
+            if not hasattr(self, k):
+                print "warning"
+            setattr(self, k, v)
+        print 'user config:'
+        for k,v in self.__class__.__dict__.items():
+            if not k.startwith('__'):
+                print k, getattr(self, k)
