4e4258ed19c93b5e8c026cb82b59c1a388c2d75c
Jarlonyan <286568450@qq.com>
2019-08-08 19:16:16 +0800

    update

--- siamese_network/main.py
+++ siamese_network/main.py
@@ -11,6 +11,11 @@ from torchvision import transforms
 import torch.optim as optim
 from torch.autograd import Variable
 import torchvision.datasets as dset
+import torch.nn.functional as F
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 import conf
 import utils
@@ -63,35 +68,38 @@ def train():
     plt.show()
     torch.save(net, 'siamese_network.pkl')  # 保存整个神经网络的结构和模型参数 
 
-def test():
-    model_path ='siameses_network.pkl'
-    model = torch.load(model_path)
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show() 
 
-    train_data = MyDataset(txt=conf.txt_train_data, 
+def test():
+    net = torch.load('siamese_network.pkl')
+    test_data = MyDataset(txt=conf.txt_test_data, 
                            transform=transforms.Compose([transforms.Resize((100, 100)), transforms.ToTensor()]),  \
                            should_invert=False)
-    train_dataloader = DataLoader(dataset=train_data, \
+    test_dataloader = DataLoader(dataset=test_data, \
                                   shuffle=True,       \
                                   batch_size=conf.train_batch_size)
-    net = siamese_network.SiameseNetwork()
-    criterion = siamese_network.ContrastiveLoss()
 
-    data_iter = iter(train_dataloader)
-    img1, img2, label = next(data_iter)
-    output1, output2 = net(img1, img2)
-    #loss_contrastive = criterion(output1, output2, label)
-    print output1
-    #print loss_contrastive
+    dataiter = iter(test_dataloader)
+    x0,_,_ = next(dataiter)
 
-    '''img1, img2, label = data
-    img1, img2, label = Variable(img1), Variable(img2), Variable(label)
-    output1, output2 = net(img1, img2)
-    loss_contrastive = criterion(output1, output2, label)
-    '''
+    for i in range(10):
+        _,x1,label2 = next(dataiter)
+        concatenated = torch.cat((x0,x1),0)
+    
+        output1,output2 = net(Variable(x0),Variable(x1))
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        imshow(torchvision.utils.make_grid(concatenated),'Dissimilarity: {:.2f}'.format(euclidean_distance.cpu().data.numpy()[0][0]))
 
 def main():
-    train()
-    #test()
+    #train()
+    test()
 
 if __name__ == "__main__":
     main()
