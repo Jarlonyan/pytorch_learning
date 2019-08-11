cf382c47a5dfe3f1b679bce7f04a556e5ed4766f
Jarlonyan <286568450@qq.com>
2019-08-11 21:12:37 +0800

    update

--- hand_classifier/conf.py
+++ hand_classifier/conf.py
@@ -3,8 +3,8 @@
 txt_train_data = 'images/train.txt'
 txt_test_data = 'images/test.txt'
 
-train_batch_size = 32
-train_number_epochs = 20
+train_batch_size = 320
+train_number_epochs = 6
 
 root = 'images/'
 
