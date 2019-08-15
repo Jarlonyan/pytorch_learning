f5af95910a66ff14f7b9668f7af2d8786f459496
Jarlonyan <286568450@qq.com>
2019-08-15 22:20:51 +0800

    update

--- gan_network/model.py
+++ gan_network/model.py
@@ -5,5 +5,66 @@ import torch.nn as nn
 
 class NetG(nn.Module):
     def __init__(self, ngf, nz):
+        super(NetG, self).__init__()
+        # layer1输入是100x1x1的随机噪声，输出(ngf*8)x4x4
+        self.layer1 = nn.Sequential(
+            nn.ConvTranspose2d(nz, ngf*8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(ngf*8),
+            nn.ReLU(inplace=True)
+        )
+        #layer2输出尺寸(ngf*4)x8x8
+        self.layer2 = nn.Sequential(
+            nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*4),
+            nn.ReLU(inplace=True)
+        )
+        #layer3输出尺寸(ngf*2)x16x16
+        self.layer3 = nn.Sequential(
+            nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*2),
+            nn.ReLU(inplace=True)
+        )
+        #layer4输出尺寸(ngf)x32x32
+        self.layer4 = nn.Sequential(
+            nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(inplae=True)
+        ) 
+        #layer5输出尺寸3x96x96
+        self.layer5 = nn.Sequential(
+            nn.ConvTranspose2d(ngf, 3, 5, 3, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        return x
+
+#判定网络D
+class NetD(nn.Module):
+    def __init__(self, ndf):
+        super(NetD, self).__init__()
+        self.layer1=  nn.Sequential(
+            nn.Conv2d(3, ndf, kernel_size=5, stride=3, padding=1, bias=False),
+            nn.BatchNorm2d(ndf),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.layer4 = nn.S
+
+
 
 
