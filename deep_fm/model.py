74554af89d32a10f1f6f61ad617b360874168f11
Jarlonyan <286568450@qq.com>
2020-01-09 14:12:33 +0800

    update

--- deep_fm/model.py
+++ deep_fm/model.py
@@ -40,4 +40,35 @@ class DeepFM(torch.nn.Module):
         fm_sum_second_order_emb_square = fm_sum_second_order_emb * fm_sum_second_order_emb
         fm_second_order_emb_square_sum = [item*item for item in fm_second_order_emb_arr]
         fm_second_order = (fm_sum_second_order_emb_square - fm_second_order_emb_square_sum) * 0.5
-        
+
+        #deep part
+        deep_emb = torch.cat(fm_second_order_emb_arr, 1)
+        deep_out = deep_emb
+        for i in range(1,len(self.hidden_dims)+1):
+            deep_out = getattr(self, 'linear_'+str(i))(deep_out)
+            deep_out = getattr(self, 'batchNorm_'+str(i))(deep_out)
+            deep_out = getattr(self, "dropout_"+str(i))(deep_out)
+
+        #sum
+        total_sum = torch.sum(fm_first_order,1) + torch.sum(fm_second_order,1) + torch.sum(deep_out,1) + self.bias
+    
+    def train_model(self, loader_train, loader_val, optimizer, epochs=10, verbose=False, print_every+10):
+        model = self.train().to(device='cpu')
+        criterion = F.binary_cross_entropy_with_logits
+
+        for _ in range(epochs):
+            for t,(xi,xv,y) in enumerate(loader_train):
+                xi = xi.to(device='cpu', dtype=self.dtype)
+                xv = xv.to(device='cpu', dtype=torch.float)
+                y = y.to(device='cpu', dtype=torch.float)
+
+                total = model(xi, xv)
+                loss = criterion(total, y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                if verbose and t%print_every==0:
+                    print ('Iteration %d,loss=%.4f' % (t, loss.item()))
+                    print ()
+
